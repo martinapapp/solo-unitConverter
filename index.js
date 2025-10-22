8b2f08e732db44ded31e89be181cd0696a0dd7dc
Martina Papp <martinapapp0@gmail.com>
2025-10-22 17:45:27 +0200

    success in js

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 /*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
+what i learnt:
+If a value changes and you need to use the new value (like user input), you must read it after the change or inside the function that handles the event.
 */
 
-const convertBtn=document.querySelector(".convert-btn");
-const inputEl=document.getElementById("input-el");
-const lengthOutput = document.querySelector(".lengthBox");
-const volumeOutput = document.querySelector(".volumeBox");
-const massOutput = document.querySelector(".massBox");
+const convertBtn=document.getElementById('convert-btn');
+const lengthBox = document.getElementById('length-box');
+const volumeBox = document.getElementById('volume-box');
+const massBox = document.getElementById('mass-box');
 const meter_feet = 3.281;
 const liter_gallon = 0.264;
 const kilo_pound = 2.204;
-let baseValue = parseFloat(inputEl.value);
 
-// Handling Error
- if (isNaN(baseValue)) {
-        alert("Please enter a valid number!");
-// Clear all output fields
-        lengthOutputEl.textContent = "";
-        volumeOutputEl.textContent = "";
-        massOutputEl.textContent = "";
- }
+convertBtn.addEventListener('click', function(){
+const inputValue = parseFloat(document.getElementById("input-el").value)
+if (isNaN(inputValue)) {
+lengthBox.textContent = "Please enter a valid number.";
+return;
+}else{
+let metersToFeet = inputValue * meter_feet;
+let feetToMeters = inputValue / meter_feet;
+lengthBox.textContent = `${inputValue} meters = ${metersToFeet.toFixed(3)} feet | feet ${inputValue} = ${feetToMeters.toFixed(3)} meters`;
 
-function convertLength(value) {
-  let feet = value * meter_feet;
-  let meters = value / meter_feet;
-  lengthOutput.textContent = `${value} meters = ${feet.toFixed(3)} feet = ${meters.toFixed(3)}`
+let litersToGallons = inputValue * liter_gallon;
+let gallonsToLiters = inputValue / liter_gallon;
+volumeBox.textContent = `${inputValue} liters = ${litersToGallons.toFixed(3)} gallons | ${inputValue} gallons = ${gallonsToLiters.toFixed(3)} liters`;
+
+let kilosToPounds = inputValue * kilo_pound;
+let poundsToKilos = inputValue / kilo_pound;
+massBox.textContent = `${inputValue} kilograms = ${kilosToPounds.toFixed(3)} pounds | ${inputValue} pounds = ${poundsToKilos.toFixed(3)} kilograms`;
 }
 
-function convertVolume(value) {
-  let gallons = value * liter_gallon;
-  let liters = value / liter_gallon;
-  volumeOutput.textContent = `${value} gallons = ${gallons.toFixed(3)} liter = ${liter.toFixed(3)}`
-}
+})
 
-function convertMass(value) {
-  let pound = value * kilo_pound;
-  let kilo = value / kilo_pound;
-  lengthOutput.textContent = `${value} meters = ${feet.toFixed(3)} feet = ${meters.toFixed(3)}`
-}
 
-convertBtn.addEventListener('click', function () {
-    convertLength(baseValue);
-    convertVolume(baseValue);
-    convertMass(baseValue);
-});
+ 
